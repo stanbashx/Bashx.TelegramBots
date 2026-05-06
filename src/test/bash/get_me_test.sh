6ee31beaa55749f79b327fbe280cfdab94a91c7e
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-05-06 18:24:34 +0300

    tests

--- src/test/bash/get_me_test.sh
+++ src/test/bash/get_me_test.sh
@@ -5,3 +5,19 @@ ISSUER="$tgbots/get_me.sh"
 ACTUAL_VALUE="$(${ISSUER})"
 . $asserts/ne.sh $? 0
 . $asserts/eq.sh "${ACTUAL_VALUE}" 'Wrong arguments!'
+
+ACTUAL_VALUE="$(${ISSUER} 0 0)"
+. $asserts/ne.sh $? 0
+. $asserts/eq.sh "${ACTUAL_VALUE}" 'Wrong arguments!'
+
+ACTUAL_VALUE="$(${ISSUER} '')"
+. $asserts/ne.sh $? 0
+. $asserts/eq.sh "${ACTUAL_VALUE}" 'Argument "TG_BOT_ID" is empty!'
+
+ACTUAL_VALUE="$(TG_BOT_ID=0 ${ISSUER} '')"
+. $asserts/ne.sh $? 0
+. $asserts/eq.sh "${ACTUAL_VALUE}" 'Argument "TG_BOT_TOKEN" is empty!'
+
+ACTUAL_VALUE="$(TG_BOT_ID=0 TG_BOT_TOKEN=0 ${ISSUER} '')"
+. $asserts/ne.sh $? 0
+. $asserts/eq.sh "${ACTUAL_VALUE}" 'Argument "TG_FILEPATH" is empty!'
