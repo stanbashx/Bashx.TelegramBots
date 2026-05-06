7f2fbdfac8413e8ddf526e911f7b7dbfa5e4cb8d
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-05-06 17:20:58 +0300

    assemble

---
+++ assemble.sh
@@ -0,0 +1,22 @@
+#!/usr/local/bin/bash
+
+mkdir -p 'build'
+mkdir -p 'build/yml'
+ISSUER='build/yml/metadata.yml'
+echo "repository:
+ owner: 'StanleyProjects'
+ name: 'TelegramBots'
+version: '0.0.1'" > "${ISSUER}"
+
+VERSION="$(yq -erM .version "${ISSUER}")" || exit 1
+REP_NAME="$(yq -erM .repository.name "${ISSUER}")" || exit 1
+
+if [[ ! -s 'LICENSE' ]]; then
+ echo 'No license!'; exit 1; fi
+
+if [[ ! -s 'README.md' ]]; then
+ echo 'No readme!'; exit 1; fi
+
+mkdir -p 'build/zip'
+ISSUER="build/zip/${REP_NAME}-${VERSION}.zip"
+zip -r "${ISSUER}" 'src/main/bash' 'LICENSE' 'README.md'
