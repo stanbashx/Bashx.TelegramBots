bad1397d70efc0c81db677e68ea39df1f285590d
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-05-06 18:22:11 +0300

    tests

--- src/test/bash/send_message_test.sh
+++ src/test/bash/send_message_test.sh
@@ -5,3 +5,23 @@ ISSUER="$tgbots/send_message.sh"
 ACTUAL_VALUE="$(${ISSUER})"
 . $asserts/ne.sh $? 0
 . $asserts/eq.sh "${ACTUAL_VALUE}" 'Wrong arguments!'
+
+ACTUAL_VALUE="$(${ISSUER} 0 0)"
+. $asserts/ne.sh $? 0
+. $asserts/eq.sh "${ACTUAL_VALUE}" 'Wrong arguments!'
+
+ACTUAL_VALUE="$(${ISSUER} '')"
+. $asserts/ne.sh $? 0
+. $asserts/eq.sh "${ACTUAL_VALUE}" 'Argument "TG_BOT_ID" is empty!'
+
+ACTUAL_VALUE="$(TG_BOT_ID=0 ${ISSUER} '')"
+. $asserts/ne.sh $? 0
+. $asserts/eq.sh "${ACTUAL_VALUE}" 'Argument "TG_BOT_TOKEN" is empty!'
+
+ACTUAL_VALUE="$(TG_BOT_ID=0 TG_BOT_TOKEN=0 ${ISSUER} '')"
+. $asserts/ne.sh $? 0
+. $asserts/eq.sh "${ACTUAL_VALUE}" 'Argument "TG_CHAT_ID" is empty!'
+
+ACTUAL_VALUE="$(TG_BOT_ID=0 TG_BOT_TOKEN=0 TG_CHAT_ID=0 ${ISSUER} '')"
+. $asserts/ne.sh $? 0
+. $asserts/eq.sh "${ACTUAL_VALUE}" 'Argument "TG_MESSAGE" is empty!'
