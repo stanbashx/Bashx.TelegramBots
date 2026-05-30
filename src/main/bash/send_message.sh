fdccee80bda66e0f7354a5934c2204e629f0bb14
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-05-31 00:49:21 +0300

    wip

--- src/main/bash/send_message.sh
+++ src/main/bash/send_message.sh
@@ -1,41 +1,53 @@
 #!/usr/local/bin/bash
 
-if test $# -ne 2; then
+if [[ $# -ne 4 ]]; then
  echo 'Wrong arguments!'; exit 1; fi
 
-TG_CHAT_ID="$1"
-TG_MESSAGE="$2"
+TGBOTS_BOT_ID="$1"
+TGBOTS_BOT_TOKEN="$2"
+TGBOTS_CHAT_ID="$3"
+TGBOTS_MESSAGE="$4"
 
-ARGUMENTS=(TG_BOT_ID TG_BOT_TOKEN TG_CHAT_ID TG_MESSAGE)
-for (( INDEX=0; INDEX<${#ARGUMENTS[@]}; INDEX++ )); do
- ARGUMENT="${ARGUMENTS[INDEX]}"
- if test -z "${!ARGUMENT}"; then
-  echo "Argument \"$ARGUMENT\" is empty!"; exit $((100+INDEX)); fi
+args=(TGBOTS_BOT_ID TGBOTS_BOT_TOKEN TGBOTS_CHAT_ID TGBOTS_MESSAGE)
+for ((i=0; i<${#args[@]}; i++ )); do
+ arg="${args[i]}"
+ if [[ -z "${!arg}" ]]; then
+  echo "\"${arg}\" is empty!"; exit 1; fi
 done
 
-if [[ ! "${TG_CHAT_ID}" =~ ^-?[0-9]+$ ]]; then
+if [[ ! "${TGBOTS_CHAT_ID}" =~ ^-?[0-9]+$ ]]; then
  echo 'Wrong chat id!'; exit 1; fi
 
-REQUEST_BODY="{
- \"parse_mode\": \"Markdown\",
- \"link_preview_options\": {
-  \"is_disabled\": true
- },
- \"chat_id\": ${TG_CHAT_ID}
-}"
+TGBOTS_PARSE_MODE='Markdown'
 
-REQUEST_BODY="$(echo "${REQUEST_BODY}" | TG_MESSAGE="${TG_MESSAGE}" yq -M -o=json '.text=strenv(TG_MESSAGE)')"
-if test $? -ne 0; then echo 'Request body error!'; exit 1; fi
+REQUEST_BODY='{}'
+
+REQUEST_BODY="$(printf '%s' "${REQUEST_BODY}" | \
+ STR_VALUE="${TGBOTS_PARSE_MODE}" \
+ yq -Me -p=json -o=json '.parse_mode=strenv(STR_VALUE)')" || exit 1
+REQUEST_BODY="$(printf '%s' "${REQUEST_BODY}" | \
+ yq -Me -p=json -o=json '.link_preview_options.is_disabled=true')" || exit 1
+REQUEST_BODY="$(printf '%s' "${REQUEST_BODY}" | \
+ yq -Me -p=json -o=json ".chat_id=${TGBOTS_CHAT_ID}")" || exit 1
+REQUEST_BODY="$(printf '%s' "${REQUEST_BODY}" | \
+ STR_VALUE="${TGBOTS_MESSAGE}" \
+ yq -Me -p=json -o=json '.text=strenv(STR_VALUE)')" || exit 1
+
+echo "${REQUEST_BODY}" | yq
+
+echo 'Not implemented!'; exit 1 # todo
+
+TGBOTS_URL="https://api.telegram.org/bot${TGBOTS_BOT_ID}:${TGBOTS_BOT_TOKEN}"
 
 # https://core.telegram.org/bots/api#sendmessage
 
 CODE=$(curl -m 8 -w %{http_code} -o /dev/null \
- "https://api.telegram.org/bot${TG_BOT_ID}:${TG_BOT_TOKEN}/sendMessage" \
+ "${TGBOTS_URL}/sendMessage" \
  -H 'Content-Type: application/json' \
  --data "${REQUEST_BODY}")
 
-if test $? -ne 0; then
- echo 'Curl error!'; exit 1; fi
-
-if [[ "${CODE}" != '200' ]]; then
- echo 'Send tg message error!'; exit 1; fi
+if [[ $? -ne 0 ]]; then
+ echo 'Curl error!'; exit 1
+elif [[ "${CODE}" != '200' ]]; then
+ echo 'Send tg message error!'; exit 1
+fi
