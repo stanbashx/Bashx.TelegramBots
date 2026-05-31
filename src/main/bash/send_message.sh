2aba2157cd6105526bb18d8b6ca5b0ae8f8d9670
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-06-01 00:47:09 +0300

    wip

--- src/main/bash/send_message.sh
+++ src/main/bash/send_message.sh
@@ -1,21 +1,29 @@
 #!/usr/local/bin/bash
 
-if [[ $# -ne 4 ]]; then
+if [[ $# -ne 5 ]]; then
  echo 'Wrong arguments!' >&2; exit 1; fi
 
 TGBOTS_BOT_ID="$1"
-TGBOTS_BOT_TOKEN="$2"
+TGBOTS_BOT_SECRET="$2"
 TGBOTS_CHAT_ID="$3"
 TGBOTS_MESSAGE="$4"
 TGBOTS_OUTPUT="$5"
 
-if [[ ! "${TGBOTS_BOT_ID}" =~ ^[1-9][0-9]{7,15}$ ]]; then
+if [[ -z "${TGBOTS_BOT_ID}" ]]; then
+ echo 'No bot id!' >&2; exit 1
+elif [[ ! "${TGBOTS_BOT_ID}" =~ ^[1-9][0-9]{7,15}$ ]]; then
  echo 'Wrong bot id!' >&2; exit 1
-elif [[ ! "${TGBOTS_BOT_TOKEN}" =~ ^AA[a-zA-Z0-9_-]{33}$ ]]; then
- echo 'Wrong bot token!' >&2; exit 1
+elif [[ -z "${TGBOTS_BOT_SECRET}" ]]; then
+ echo 'No bot secret!' >&2; exit 1
+elif [[ ! "${TGBOTS_BOT_SECRET}" =~ ^AA[a-zA-Z0-9_-]{33}$ ]]; then
+ echo 'Wrong bot secret!' >&2; exit 1
+elif [[ -z "${TGBOTS_CHAT_ID}" ]]; then
+ echo 'No chat id!' >&2; exit 1
 elif [[ ! "${TGBOTS_CHAT_ID}" =~ ^-?[1-9][0-9]*$ ]]; then
  echo 'Wrong chat id!' >&2; exit 1
-elif [[ -z "${TGBOTS_MESSAGE}" || "${#TGBOTS_MESSAGE}" -gt 4096 ]]; then
+elif [[ -z "${TGBOTS_MESSAGE}" ]]; then
+ echo 'No message!' >&2; exit 1
+elif [[ "${#TGBOTS_MESSAGE}" -gt 4096 ]]; then
  echo 'Wrong message size!' >&2; exit 1
 fi
 
@@ -40,7 +48,7 @@ TGBOTS_REQUEST_BODY="$(printf '%s' "${TGBOTS_REQUEST_BODY}" | \
  STR_VALUE="${TGBOTS_MESSAGE}" \
  yq -Me -p=json -o=json '.text=strenv(STR_VALUE)')" || exit 1
 
-TGBOTS_URL="https://api.telegram.org/bot${TGBOTS_BOT_ID}:${TGBOTS_BOT_TOKEN}"
+TGBOTS_URL="https://api.telegram.org/bot${TGBOTS_BOT_ID}:${TGBOTS_BOT_SECRET}"
 
 # https://core.telegram.org/bots/api#sendmessage
 
@@ -64,9 +72,9 @@ elif [[ ! -s "${TGBOTS_OUTPUT}" ]]; then
  echo "\"${TGBOTS_OUTPUT}\" is empty!" >&2; exit 1
 fi
 
-TGBOTS_CHECKS="$(yq -Mer '.ok // false' "${TG_OUTPUT}" 2>/dev/null)"
+TGBOTS_CHECKS="$(yq -Mer '.ok // false' "${TGBOTS_OUTPUT}" 2>/dev/null)"
 if [[ $? -ne 0 ]]; then
- echo 'Parse output error!'; exit 1
+ echo 'Parse output error!' >&2; exit 1
 elif [[ "${TGBOTS_CHECKS}" != 'true' ]]; then
- echo 'Check output error!'; exit 1
+ echo 'Check output error!' >&2; exit 1
 fi
