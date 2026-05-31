9770335a065344dc97e5789d195fccff768adc50
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-06-01 00:30:43 +0300

    wip

--- src/main/bash/send_message.sh
+++ src/main/bash/send_message.sh
@@ -7,6 +7,7 @@ TGBOTS_BOT_ID="$1"
 TGBOTS_BOT_TOKEN="$2"
 TGBOTS_CHAT_ID="$3"
 TGBOTS_MESSAGE="$4"
+TGBOTS_OUTPUT="$5"
 
 if [[ ! "${TGBOTS_BOT_ID}" =~ ^[1-9][0-9]{7,15}$ ]]; then
  echo 'Wrong bot id!' >&2; exit 1
@@ -16,6 +17,18 @@ elif [[ ! "${TGBOTS_CHAT_ID}" =~ ^-?[1-9][0-9]*$ ]]; then
  echo 'Wrong chat id!' >&2; exit 1
 elif [[ -z "${TGBOTS_MESSAGE}" || "${#TGBOTS_MESSAGE}" -gt 4096 ]]; then
  echo 'Wrong message size!' >&2; exit 1
+fi
+
+if [[ -z "${TGBOTS_OUTPUT}" ]]; then
+ echo 'No output!' >&2; exit 1
+elif [[ -L "${TGBOTS_OUTPUT}" ]]; then
+ echo "\"${TGBOTS_OUTPUT}\" is a symlink!" >&2; exit 1
+elif [[ -e "${TGBOTS_OUTPUT}" ]]; then
+ if [[ -f "${TGBOTS_OUTPUT}" ]]; then
+  echo "\"${TGBOTS_OUTPUT}\" exists!" >&2; exit 1
+ else
+  echo "\"${TGBOTS_OUTPUT}\" is not a file!" >&2; exit 1
+ fi
 fi
 
 TGBOTS_REQUEST_BODY="{
@@ -31,14 +44,29 @@ TGBOTS_URL="https://api.telegram.org/bot${TGBOTS_BOT_ID}:${TGBOTS_BOT_TOKEN}"
 
 # https://core.telegram.org/bots/api#sendmessage
 
-CODE=$(curl -m 8 -w '%{http_code}' -o /dev/null \
+CODE=$(curl -m 8 -w '%{http_code}' \
  "${TGBOTS_URL}/sendMessage" \
  -H 'Content-Type: application/json' \
  --data "${TGBOTS_REQUEST_BODY}" \
- 2>/dev/null)
+ -o "${TGBOTS_OUTPUT}" 2>/dev/null)
 
 if [[ $? -ne 0 ]]; then
- echo 'Curl error!' >&2; exit 1
+ echo 'Request error!' >&2; exit 1
 elif [[ "${CODE}" != '200' ]]; then
  echo 'Send tg message error!' >&2; exit 1
+fi
+
+if [[ ! -e "${TGBOTS_OUTPUT}" ]]; then
+ echo "\"${TGBOTS_OUTPUT}\" does not exist!" >&2; exit 1
+elif [[ ! -f "${TGBOTS_OUTPUT}" ]]; then
+ echo "\"${TGBOTS_OUTPUT}\" is not a file!" >&2; exit 1
+elif [[ ! -s "${TGBOTS_OUTPUT}" ]]; then
+ echo "\"${TGBOTS_OUTPUT}\" is empty!" >&2; exit 1
+fi
+
+TGBOTS_CHECKS="$(yq -Mer '.ok // false' "${TG_OUTPUT}" 2>/dev/null)"
+if [[ $? -ne 0 ]]; then
+ echo 'Parse output error!'; exit 1
+elif [[ "${TGBOTS_CHECKS}" != 'true' ]]; then
+ echo 'Check output error!'; exit 1
 fi
