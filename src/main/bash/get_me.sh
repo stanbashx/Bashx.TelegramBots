f1fe4d246109c4acf2c239c53ba57353d90f8793
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-06-03 17:05:17 +0300

    wip

--- src/main/bash/get_me.sh
+++ src/main/bash/get_me.sh
@@ -5,7 +5,7 @@ if [[ $# -ne 3 ]]; then
 
 TGBOTS_BOT_ID="$1"
 TGBOTS_BOT_SECRET="$2"
-TGBOTS_OUTPUT="$3"
+TGBOTS_DST="$3"
 
 if [[ -z "${TGBOTS_BOT_ID}" ]]; then
  echo 'No bot id!' >&2; exit 1
@@ -17,15 +17,15 @@ elif [[ ! "${TGBOTS_BOT_SECRET}" =~ ^[a-zA-Z0-9_-]{35}$ ]]; then
  echo 'Wrong bot secret!' >&2; exit 1
 fi
 
-if [[ -z "${TGBOTS_OUTPUT}" ]]; then
- echo 'No output!' >&2; exit 1
-elif [[ -L "${TGBOTS_OUTPUT}" ]]; then
- echo "\"${TGBOTS_OUTPUT}\" is a symlink!" >&2; exit 1
-elif [[ -e "${TGBOTS_OUTPUT}" ]]; then
- if [[ -f "${TGBOTS_OUTPUT}" ]]; then
-  echo "\"${TGBOTS_OUTPUT}\" exists!" >&2; exit 1
+if [[ -z "${TGBOTS_DST}" ]]; then
+ echo 'No dst!' >&2; exit 1
+elif [[ -L "${TGBOTS_DST}" ]]; then
+ echo "\"${TGBOTS_DST}\" is a symlink!" >&2; exit 1
+elif [[ -e "${TGBOTS_DST}" ]]; then
+ if [[ -f "${TGBOTS_DST}" ]]; then
+  echo "\"${TGBOTS_DST}\" exists!" >&2; exit 1
  else
-  echo "\"${TGBOTS_OUTPUT}\" is not a file!" >&2; exit 1
+  echo "\"${TGBOTS_DST}\" is not a file!" >&2; exit 1
  fi
 fi
 
@@ -35,7 +35,7 @@ TGBOTS_URL="https://api.telegram.org/bot${TGBOTS_BOT_ID}:${TGBOTS_BOT_SECRET}"
 
 HTTP_CODE=$(curl -m 8 -w '%{http_code}' \
  "${TGBOTS_URL}/getMe" \
- -o "${TGBOTS_OUTPUT}" 2>/dev/null)
+ -o "${TGBOTS_DST}" 2>/dev/null)
 
 if [[ $? -ne 0 ]]; then
  echo 'Request error!' >&2; exit 1
@@ -43,28 +43,28 @@ elif [[ "${HTTP_CODE}" != '200' ]]; then
  echo 'Code error!' >&2; exit 1
 fi
 
-if [[ -L "${TGBOTS_OUTPUT}" ]]; then
- echo "\"${TGBOTS_OUTPUT}\" is a symlink!" >&2; exit 1
-elif [[ ! -e "${TGBOTS_OUTPUT}" ]]; then
- echo "\"${TGBOTS_OUTPUT}\" does not exist!" >&2; exit 1
-elif [[ ! -f "${TGBOTS_OUTPUT}" ]]; then
- echo "\"${TGBOTS_OUTPUT}\" is not a file!" >&2; exit 1
-elif [[ ! -s "${TGBOTS_OUTPUT}" ]]; then
- echo "\"${TGBOTS_OUTPUT}\" is empty!" >&2; exit 1
+if [[ -L "${TGBOTS_DST}" ]]; then
+ echo "\"${TGBOTS_DST}\" is a symlink!" >&2; exit 1
+elif [[ ! -e "${TGBOTS_DST}" ]]; then
+ echo "\"${TGBOTS_DST}\" does not exist!" >&2; exit 1
+elif [[ ! -f "${TGBOTS_DST}" ]]; then
+ echo "\"${TGBOTS_DST}\" is not a file!" >&2; exit 1
+elif [[ ! -s "${TGBOTS_DST}" ]]; then
+ echo "\"${TGBOTS_DST}\" is empty!" >&2; exit 1
 fi
 
-TGBOTS_OUTPUT_TAGS="$(yq -Mer -p=json -o=json 'tag' "${TGBOTS_OUTPUT}" 2>/dev/null)"
-if [[ $? -ne 0 || "${TGBOTS_OUTPUT_TAGS}" != '!!map' ]]; then
- echo 'Parse output error!' >&2; exit 1; fi
+TGBOTS_DST_TAGS="$(yq -Mer -p=json -o=json 'tag' "${TGBOTS_DST}" 2>/dev/null)"
+if [[ $? -ne 0 || "${TGBOTS_DST_TAGS}" != '!!map' ]]; then
+ echo 'Parse dst error!' >&2; exit 1; fi
 
-TGBOTS_CHECKS="$(yq -M -p=json -o=json '.ok // false' "${TGBOTS_OUTPUT}" 2>/dev/null)"
+TGBOTS_CHECKS="$(yq -M -p=json -o=json '.ok // false' "${TGBOTS_DST}" 2>/dev/null)"
 if [[ "${TGBOTS_CHECKS}" != 'true' ]]; then
- echo 'Check output error!' >&2; exit 1; fi
+ echo 'Check dst error!' >&2; exit 1; fi
 
-RESPONSE_BOT_ID="$(yq -Mr -p=json -o=json '.result.id // ""' "${TGBOTS_OUTPUT}" 2>/dev/null)"
+RESPONSE_BOT_ID="$(yq -Mr -p=json -o=json '.result.id // ""' "${TGBOTS_DST}" 2>/dev/null)"
 if [[ "${TGBOTS_BOT_ID}" != "${RESPONSE_BOT_ID}" ]]; then
  echo 'Check bot id error!' >&2; exit 1; fi
 
-RESPONSE_IS_BOT="$(yq -M -p=json -o=json '.result.is_bot // false' "${TGBOTS_OUTPUT}" 2>/dev/null)"
+RESPONSE_IS_BOT="$(yq -M -p=json -o=json '.result.is_bot // false' "${TGBOTS_DST}" 2>/dev/null)"
 if [[ "${RESPONSE_IS_BOT}" != 'true' ]]; then
  echo 'Check bot error!' >&2; exit 1; fi
