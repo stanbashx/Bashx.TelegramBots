68a0c8a48b8364d155e806f6c4c1f9298def7259
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-19 00:20:38 +0300

    assemble

--- assemble.sh
+++ assemble.sh
@@ -1,11 +1,11 @@
 #!/usr/local/bin/bash
 
-REP_OWNER='StanleyProjects'
-REP_NAME='TelegramBots'
+REP_OWNER='stanbashx'
+REP_NAME='Bashx.TelegramBots'
 VERSION='0.0.8'
 
 if [[ -d 'build' ]]; then
- echo 'Build dir exists!'; exit 1; fi
+ echo 'Build dir exists!' >&2; exit 1; fi
 
 mkdir 'build'
 mkdir -p 'build/yml'
@@ -16,13 +16,13 @@ echo "repository:
 version: '${VERSION}'" > "${ISSUER}"
 
 if [[ ! -s 'LICENSE' ]]; then
- echo 'No license!'; exit 1; fi
+ echo 'No license!' >&2; exit 1; fi
 
 if [[ ! -s 'README.md' ]]; then
- echo 'No readme!'; exit 1; fi
+ echo 'No readme!' >&2; exit 1; fi
 
 mkdir -p 'build/zip'
 ISSUER="build/zip/${REP_NAME}-${VERSION}.zip"
 zip -r "${ISSUER}" 'src/main/bash' 'LICENSE' 'README.md'
 if [[ $? -ne 0 ]]; then
- echo 'Zip error!'; exit 1; fi
+ echo 'Zip error!' >&2; exit 1; fi
